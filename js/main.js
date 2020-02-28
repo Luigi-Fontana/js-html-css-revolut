a5b02cfb47f3e1b6ade31e26f222079d3915e79e
Luigi Fontana <luigifontana92@gmail.com>
2020-02-28 17:19:49 +0100

    Aggiunta della freccetta bonus

--- js/main.js
+++ js/main.js
@@ -8,11 +8,12 @@ $(document).ready(function(){
     $('.menu-item').click(function(){
         if ($(this).children('div').is(':visible')){
             $(this).children('div').slideToggle(100);
-            $(this).children('a').toggleClass('ruota');
+            $('.menu-item').children('a').removeClass('ruota');
         } else {
             $('.menu-item').children('div').slideUp(100);
             $(this).children('div').slideToggle(100);
             $('.menu-item').children('a').removeClass('ruota');
+            $(this).children('a').addClass('ruota');
         }
     });
 });
