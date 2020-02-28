5ef3dae2c6040b155d04d2e463d14892dc761e4f
Luigi Fontana <luigifontana92@gmail.com>
2020-02-28 17:02:25 +0100

    Aggiunta di JQuery

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
-/*
-
-*/
 $(document).ready(function(){
-    
+    $('.menu-item').mouseenter(function(){
+        $(this).addClass('selected');
+    });
+    $('.menu-item').mouseleave(function(){
+        $(this).removeClass('selected');
+    });
+    $('.menu-item').click(function(){
+        if ($(this).children('div').is(':visible')){
+            $(this).children('div').slideToggle(100);
+            $(this).children('a').toggleClass('ruota');
+        } else {
+            $('.menu-item').children('div').slideUp(100);
+            $(this).children('div').slideToggle(100);
+            $('.menu-item').children('a').removeClass('ruota');
+        }
+    });
 });
